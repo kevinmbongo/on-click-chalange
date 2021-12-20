050f596d6b19a9d6f5f0f6141fd352d622abd53a
kevinm <mbongo.kevin.pro@gmail.com>
2021-12-20 17:13:38 +0100

    chore(project): finish project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
-  <div id="app">Hello World</div>
+  <div class="bg-red-400 w-full h-screen flex items-center justify-center">
+    <img
+      class="w-1/2"
+      :src="require(`./assets/images/${imageSrc}`)"
+      :alt="imageSrc"
+      @click="changeOnClick"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+
+  data: () => ({
+    imageSrc: 'chat-lunette.jpeg',
+  }),
+
+  methods: {
+    changeOnClick() {
+      const imageList = [
+        'chat-lunette.jpeg',
+        'mongolfiere.jpg',
+        'orange.jpeg',
+        'papillon.jpg',
+      ]
+
+      const randomIndex = Math.floor(Math.random() * imageList.length)
+
+      if (imageList[randomIndex] === this.imageSrc) {
+        this.changeOnClick()
+      } else {
+        this.imageSrc = imageList[randomIndex]
+      }
+    },
+  },
 }
 </script>
